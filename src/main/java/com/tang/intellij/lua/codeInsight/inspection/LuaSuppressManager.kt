efaf718902b1e64a4907d461d47eefe2ece928cf
RedNesto <RedNestoMC@gmail.com>
2022-05-07 13:11:48 +0200

    Implement suppress inspection quickfix

--- src/main/java/com/tang/intellij/lua/codeInsight/inspection/LuaSuppressManager.kt
+++ src/main/java/com/tang/intellij/lua/codeInsight/inspection/LuaSuppressManager.kt
@@ -17,22 +17,27 @@
 package com.tang.intellij.lua.codeInsight.inspection
 
 import com.intellij.codeInspection.InspectionSuppressor
+import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.SuppressQuickFix
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiWhiteSpace
 import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.util.parentOfType
 import com.tang.intellij.lua.comment.psi.LuaDocTagSuppress
 import com.tang.intellij.lua.comment.psi.LuaDocTypes
 import com.tang.intellij.lua.psi.LuaCommentOwner
 
 class LuaSuppressManager : InspectionSuppressor {
-    override fun getSuppressActions(element: PsiElement?, toolId: String): Array<SuppressQuickFix> {
-        return emptyArray()
-    }
+    override fun getSuppressActions(element: PsiElement?, toolId: String): Array<SuppressQuickFix> =
+        arrayOf(LuaSuppressQuickFix(toolId))
 
     override fun isSuppressedFor(element: PsiElement, toolId: String): Boolean {
-        val commentOwner = if (element is LuaCommentOwner) element else PsiTreeUtil.getParentOfType(element, LuaCommentOwner::class.java)
-        val suppress = commentOwner?.comment?.findTag(LuaDocTagSuppress::class.java)
-        if (suppress != null) {
+        val commentOwner = element.parentOfType<LuaCommentOwner>(true)
+        val suppresses = commentOwner?.comment?.findTags(LuaDocTagSuppress::class.java)
+            ?: return false
+        for (suppress in suppresses) {
             var child = suppress.firstChild
             while (child != null) {
                 if (child.node.elementType == LuaDocTypes.ID) {
@@ -44,5 +49,39 @@ class LuaSuppressManager : InspectionSuppressor {
             }
         }
         return false
+    }
+
+    private class LuaSuppressQuickFix(private val toolId: String) : SuppressQuickFix {
+
+        override fun getFamilyName(): String = "Suppress inspection"
+
+        override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
+            val document = PsiDocumentManager.getInstance(project).getDocument(descriptor.psiElement.containingFile)
+                ?.takeIf { it.isWritable }
+                ?: return
+            val commentOwner = getCommentOwner(descriptor.psiElement)
+                ?: return
+
+            val suppress = commentOwner.comment?.findTag(LuaDocTagSuppress::class.java)
+            if (suppress != null) {
+                document.insertString(suppress.textRange.endOffset, ",$toolId")
+            } else {
+                document.insertString(commentOwner.textRange.startOffset, "---@suppress $toolId\n")
+            }
+        }
+
+        override fun isAvailable(project: Project, context: PsiElement): Boolean =
+            context.isValid && context.isWritable && getCommentOwner(context) != null
+
+        private fun getCommentOwner(context: PsiElement): LuaCommentOwner? {
+            val adjustedContext = if (context is PsiWhiteSpace) {
+                PsiTreeUtil.skipWhitespacesBackward(context) ?: context
+            } else {
+                context
+            }
+            return adjustedContext.parentOfType(true)
+        }
+
+        override fun isSuppressAll(): Boolean = toolId.equals("ALL", true)
     }
 }
